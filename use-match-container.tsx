a4b907f5dc2902df1e57ef03eb6c4bcad62edf45
NghiepDev <me@nghiep.dev>
2025-09-27 17:25:12 +0700

    refactor: migrate to modern ES6 and improve package structure

--- use-match-container.tsx
+++ use-match-container.tsx
@@ -1,8 +1,4 @@
-if (typeof window !== 'undefined') {
-  import('match-container');
-}
-
-import {useEffect, useRef, useState} from 'react';
+import {useCallback, useState} from 'react';
 
 type Tuple<T, N extends number> = N extends N
   ? number extends N
@@ -15,49 +11,54 @@ type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N
 
 export function useMatchContainer<T extends string[]>(queries: T) {
   const [matches, setMatches] = useState(() => queries.map(() => false));
-  const eleRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const containerQueries = queries.map(
-      (query) => [eleRef.current?.matchContainer(query), query] as const
-    );
+  const eleCallbackRef = useCallback(
+    async (node: HTMLElement | null) => {
+      await import('match-container');
+      if (node instanceof HTMLElement) {
+        const containerQueries = queries.map(
+          (query) => [node.matchContainer(query), query] as const
+        );
 
-    function handle(e: Event) {
-      const event = e as ContainerQueryListEvent;
-      setMatches((prev) => {
-        const newMatches = [...prev];
-        const queryIndex = queries.indexOf(event.container);
-        if (queryIndex !== -1) {
-          newMatches[queryIndex] = event.matches;
+        function handle(e: Event) {
+          const event = e as ContainerQueryListEvent;
+          setMatches((prev) => {
+            const newMatches = [...prev];
+            const queryIndex = queries.indexOf(event.container);
+            if (queryIndex !== -1) {
+              newMatches[queryIndex] = event.matches;
+            }
+            return newMatches;
+          });
         }
-        return newMatches;
-      });
-    }
 
-    containerQueries.forEach(([containerQuery, query]) => {
-      setMatches((prev) => {
-        const newMatches = [...prev];
-        const queryIndex = queries.indexOf(query);
-        if (queryIndex !== -1) {
-          newMatches[queryIndex] = containerQuery
-            ? containerQuery.matches
-            : false;
-        }
-        return newMatches;
-      });
+        containerQueries.forEach(([containerQuery, query]) => {
+          setMatches((prev) => {
+            const newMatches = [...prev];
+            const queryIndex = queries.indexOf(query);
+            if (queryIndex !== -1) {
+              newMatches[queryIndex] = containerQuery
+                ? containerQuery.matches
+                : false;
+            }
+            return newMatches;
+          });
 
-      containerQuery?.addEventListener('change', handle);
-    });
+          containerQuery?.addEventListener('change', handle);
+        });
 
-    return () => {
-      containerQueries.forEach(([containerQuery]) => {
-        containerQuery?.removeEventListener('change', handle);
-      });
-    };
-  }, [queries]);
+        return () => {
+          containerQueries.forEach(([containerQuery]) => {
+            containerQuery?.removeEventListener('change', handle);
+          });
+        };
+      }
+    },
+    [queries]
+  );
 
   return {
-    ref: eleRef,
+    ref: eleCallbackRef,
     matches: matches as Tuple<boolean, T['length']>,
   };
 }
